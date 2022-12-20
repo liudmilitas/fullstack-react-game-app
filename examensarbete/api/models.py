6309e7fe0769481c56554c5efb20ab8c74605231
[Liudmila] <83576552+liudmilitas@users.noreply.github.com>
2022-12-20 21:25:09 +0100

    add model class for stickers

--- examensarbete/api/models.py
+++ examensarbete/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Sticker():
+
+# Fields
+   name = models.CharField(max_length=50)
+   url = models.CharField(max_length=200)
+   character = models.CharField(max_length=50)
+   description = models.TextField(max_length=200, null=True, blank=True)
+
+# Metadata
+
+# Methods
+
+class UserInfo():
+# Fields
+# user = models.OneToOneField(User, on_delete=models.CASCADE)
+   region = models.CharField(max_length=50)
+   
+
+# Metadata
+
+# Methods
