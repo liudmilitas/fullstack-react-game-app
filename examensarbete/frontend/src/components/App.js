b784292e4f82281ad7a29424c781501f4e5d31e8
[Liudmila] <83576552+liudmilitas@users.noreply.github.com>
2022-12-27 13:53:18 +0100

    setup react.js and tailwind

---
+++ examensarbete/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>
+        <p>Hello</p>
+    </div>
+  )
+}
+
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
