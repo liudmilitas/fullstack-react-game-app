5952d8e4de8b686a24edb9b4c691a5eea641c7fa
[Liudmila] <83576552+liudmilitas@users.noreply.github.com>
2023-01-20 14:57:39 +0100

    setup user backend views, login/register

--- examensarbete/api/urls.py
+++ examensarbete/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register', views.registerUser, name='register'),
     path('stickers/', views.getStickers, name="stickers"),
     path('stickers/<str:pk>/', views.getSticker, name="sticker"),
+    path('users/profile', views.getUser, name="user"),
+    path('users/', views.getUsers, name="users"),
 ]
--- examensarbete/api/views.py
+++ examensarbete/api/views.py
@@ -1,26 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Sticker
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .models import Sticker
-from .serializers import StickerSerializer
+from .models import Sticker, User
+from .serializers import StickerSerializer, UserSerializer, UserSerializerWithToken
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from django.contrib.auth.hashers import make_password
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/stickers/',
-        '/api/stickers/<id>',
-
-        '/api/stickers/create',
-        '/api/stickers/delete/<id>',
-        '/api/stickers/<update>/<id>',
-    ]
-    return Response(routes)
+# Sticker views
 
 @api_view(['GET'])
 def getStickers(request):
@@ -32,4 +25,45 @@ def getStickers(request):
 def getSticker(request, pk):
     sticker = Sticker.objects.get(_id=pk)
     serializer = StickerSerializer(sticker, many=False)
+    return Response(serializer.data)
+
+# User views
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUser(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    user = User.objects.create(
+        first_name=['name'],
+        username=['email'],
+        email=['email'],
+        password=make_password(data['password'])
+        )
+    serializer = UserSerializerWithToken(user, many=False)
     return Response(serializer.data)
