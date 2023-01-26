ab9c9823d487dd21ea186a299ae57674d7586e66
[Liudmila] <83576552+liudmilitas@users.noreply.github.com>
2023-01-26 15:57:59 +0100

    fix table bug

--- examensarbete/frontend/src/components/gacha/CharacterInfo.js
+++ examensarbete/frontend/src/components/gacha/CharacterInfo.js
@@ -30,14 +30,14 @@ export default function CharacterInfo({ currentCharacter, toggleModal }) {
 
   return (
     <>
-      {currentCharacter ? (
+      {character && currentCharacter ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none w-full">
             (
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none p-4">
                 <div className="flex w-full justify-between align-top items-start">
-                  <div className="flex flex-col">
+                  <div className="flex flex-col py-4 px-4">
                     <div className="flex">
                       <h3 className="text-2xl">{character?.name}</h3>
                       <span className="px-2">{rarity}</span>
@@ -53,21 +53,36 @@ export default function CharacterInfo({ currentCharacter, toggleModal }) {
                 <table className="table-auto mx-0">
                   <thead>
                     <tr>
-                      <th className="whitespace-nowrap">Character Info</th>
+                      <th className="whitespace-nowrap bg-indigo-100 border text-left px-4 lg:px-8 py-4">
+                        Character Info
+                      </th>
+                      <th className="whitespace-nowrap bg-indigo-100 border text-left px-4 lg:px-8 py-4"></th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td>Vision:</td>
-                      <td>{character?.vision}</td>
+                      <td className="border px-4 lg:px-8 py-4 font-bold">
+                        Vision:
+                      </td>
+                      <td className="border px-4 lg:px-8 py-4">
+                        {character?.vision}
+                      </td>
                     </tr>
                     <tr>
-                      <td>Weapon:</td>
-                      <td>{character?.weapon}</td>
+                      <td className="border px-4 lg:px-8 py-4 font-bold">
+                        Weapon:
+                      </td>
+                      <td className="border px-4 lg:px-8 py-4">
+                        {character?.weapon}
+                      </td>
                     </tr>
                     <tr>
-                      <td>Description:</td>
-                      <td>{character?.description}</td>
+                      <td className="border px-4 lg:px-8 py-4 font-bold">
+                        Description:
+                      </td>
+                      <td className="border px-4 lg:px-8 py-4">
+                        {character?.description}
+                      </td>
                     </tr>
                   </tbody>
                 </table>
--- examensarbete/frontend/src/components/pages/Shop.js
+++ examensarbete/frontend/src/components/pages/Shop.js
@@ -19,7 +19,7 @@ export default function Shop() {
         <ul className="flex flex-col lg:flex-row md:flex-row w-full justify-center items-center content-center gap-8">
           {products.map((product) => (
             <li
-              className="bg-[url('/src/image/product-bg.png')] w-[190px] h-[190px] bg-cover transition-all ease-out duration-500 hover:scale-110 hover:drop-shadow-[5px_5px_5px_rgba(79,70,229,0.9)]"
+              className="bg-[url('/src/image/product-bg.png')] w-[190px] h-[190px] bg-cover transition-all ease-out duration-500 hover:scale-110 hover:drop-shadow-2xl"
               key={product.name}
             >
               <button
@@ -30,8 +30,10 @@ export default function Shop() {
                 className="flex flex-col items-center content-center justify-end w-full h-full"
               >
                 <img className="h-24" src={product.image} />
-                <span className="pb-3">{product.name}</span>
-                <span className="pb-3">{product.price} SEK</span>
+                <span className="pb-3 text-lg">{product.name}</span>
+                <span className="pb-3 text-xl font-bold">
+                  {product.price} SEK
+                </span>
               </button>
             </li>
           ))}
